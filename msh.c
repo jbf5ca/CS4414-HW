25a5f9da9950ee667b2827b2266bfe100d6af8b0
Ben Fultz <jbf5ca@virginia.edu>
2017-09-17 03:09:12 -0400

    tokenizing input strings

--- msh.c
+++ msh.c
@@ -1,21 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <string.h> 
 
 void input_loop() {
-  char line [103];
-  char* tokens[50];
-  while(1) {
+  char line[103];
+  char* tokens[51];
+  int tcount, i;
+  while (1) {
     printf("> ");
     fgets(line, 103, stdin);
-    printf("%lu\n", strlen(line));
-    if(strlen(line) > 101) {
+    //printf("%lu\n", strlen(line));
+    if (strlen(line) > 101) {
+      // ERROR MESSAGE GOES HERE
       break;
     }
-    if(strcmp(line, "exit\n") == 0) {
+    if (strcmp(line, "exit\n") == 0) {
       break;
     }
-    printf("%s", line);
+    //printf("%s", line);
+    char* token = strtok(line, " ");
+    tcount = 0;
+    while (token != NULL) {
+      tokens[tcount] = token;
+      token = strtok(NULL, " ");
+      tcount++;
+    }
+    printf("%s\nargs:", tokens[0]);
+    for (i = 1; i < tcount; i++) {
+      printf(" %s", tokens[i]);
+    }
   }
 }
 
