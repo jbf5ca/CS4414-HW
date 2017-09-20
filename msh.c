879b92e7b7c3e9dec9bfaf7bd247acc861b737c3
Ben Fultz <jbf5ca@virginia.edu>
2017-09-20 19:33:32 -0400

    kinda working pipe parsing

--- msh.c
+++ msh.c
@@ -6,7 +6,6 @@
  * compile: make
  * all code in msh.c, executable: msh
  */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,29 +13,25 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-
 char linecset[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-./_ <>|";
 char wordcset[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-./_";
 char controls[] = "<>|";
-
-void pipe_exec(char** commands[], int groupcount, int in_redir, int out_redir, char infile[], char outfile[], int tcount) {
-  //int i,j;
-  //if (in_redir) { printf("{%s} ", infile); }
-  //for (i = 0; i < groupcount; i++) {
-  //j = 0;
-  //while (commands[i][j] != NULL) {
-  //printf("[%s] ", commands[i][j]);
-  //j++;
-  //}
-  //printf("| ");
-  //}
-  printf("[%s][%s] | [%s] | [%s] [%s] [%s]", commands[0][0], commands[0][1], commands[1][0], commands[2][0], commands[2][1], commands[2][2]);
-      }
-
+void pipe_exec(char commands[100][100][100], int groupcount, int in_redir, int out_redir, char infile[], char outfile[], int tcount) {
+  int i,j;
+  if (in_redir) { printf("{%s} ", infile); }
+  for (i = 0; i < groupcount; i++) {
+    j = 0;
+    while (strlen(commands[i][j]) > 0){// != NULL) {
+      printf("[%s]", commands[i][j]);
+      j++;
+    }
+    if (i < groupcount - 1) { printf("| "); }
+  }
+  if (out_redir) { printf("{%s}\n", outfile); }
+}
 void io_redir(char* argv[], int in_redir, int out_redir, char infile[], char outfile[], int tcount) {
   int fd;
   char rin[100], rout[100], tmp[100];
-
   /// if the command is a relative path, prepend cwd to make it absolute
   if (argv[0][0] != '/') {
     strcpy(tmp, argv[0]);
@@ -56,20 +51,16 @@ void io_redir(char* argv[], int in_redir, int out_redir, char infile[], char out
   }
   pid_t pid = fork();
   int status;
-  
   if (pid == -1) {
     perror("fork");
     exit(EXIT_FAILURE);
   }
-
   if (pid == 0) { /// child process
-    
     if (in_redir) {
       fd = open(rin, O_RDONLY);
       dup2(fd, 0);
       close(fd);
     }
-
     if (out_redir) {
       fd = open(rout, O_WRONLY | O_CREAT, 0666);
       dup2(fd, 1);
@@ -79,12 +70,10 @@ void io_redir(char* argv[], int in_redir, int out_redir, char infile[], char out
     _Exit(EXIT_FAILURE);
   } else { /// parent
     waitpid(pid, &status, 0);
-    printf("%s exited with status %d\n", argv[0], WEXITSTATUS(status));
+    printf("%s exited with exit code %d\n", argv[0], WEXITSTATUS(status));
   }
 }
-
 void cmd_exec(char* argv[], int cmd_count, int tcount) {
-
   /// if the command is a relative path, prepend cwd to make it absolute
   char tmp[100];
   if (argv[0][0] != '/') {
@@ -93,7 +82,6 @@ void cmd_exec(char* argv[], int cmd_count, int tcount) {
     strcat(argv[0], "/");
     strcat(argv[0], tmp);
   }
-  
   pid_t pid = fork();
   int status;
   if (pid == -1) {
@@ -105,7 +93,7 @@ void cmd_exec(char* argv[], int cmd_count, int tcount) {
     _Exit(EXIT_FAILURE);
   } else { /// parent
     waitpid(pid, &status, 0);
-    printf("%s exited with status %d\n", argv[0], WEXITSTATUS(status));
+    printf("%s exited with exit code %d\n", argv[0], WEXITSTATUS(status));
   }
 }
 
@@ -113,28 +101,38 @@ void input_loop() {
   //const char *pipe = "|";
   const char *space = " ";
   while (1) {
-    char line[103], line2[104];
+    char line[103];//, line2[104];
     //char* tokengroups[50];
     char* tokens[51];
+    //char tokens[51][100];
     int i, j, groupcount, tcount, err;
     int in_redir = 0, out_redir = 0;
     char infile[100];
     char outfile[100];
-    printf("> ");
-    fgets(line, 103, stdin);
-    strcpy(line2, line);
+    printf(">");
+    
+    /// read the input line and check for the end of file
+    if (fgets(line, 103, stdin) == NULL) {
+      break;
+    }
+
+    /// make sure input line is 100 characters or less
     if (strlen(line) > 101) {
       perror("Input line too long");
       continue;
     }
-    if (strcmp(line, "exit\n") == 0 || strcmp(line, (const char*)EOF) == 0) {
+
+    /// check for the exit command to terminate shell 
+    if (strcmp(line, "exit\n") == 0 ) { 
       break;
     }
+
     /// check that the line only has legal characters
     if (strspn(line, linecset) != strlen(line) - 1) {
       perror("line has illegal chars\n");
       continue;
     }
+
     /// check that no token group is both preceded by a pipe operator and has an input file redirection
     if (strchr(line, '|') != NULL && strchr(line, '<') != NULL) {
       if (strchr(line, '|') < strchr(line, '<')) {
@@ -142,6 +140,7 @@ void input_loop() {
 	continue;
       }
     }
+
     /// check that no token group is both followed by a pipe operator and has an output file redirection
     if (strrchr(line, '|') != NULL && strrchr(line, '>') != NULL) {
       if (strrchr(line, '|') > strrchr(line, '>')) {
@@ -149,6 +148,7 @@ void input_loop() {
 	continue;
       }
     }
+
     /// check that there isn't an attempt to redirect input after an output redirection
     if (strchr(line, '<') != NULL && strchr(line, '>') != NULL) {
       if (strchr(line, '>') < strchr(line, '<')) {
@@ -156,6 +156,7 @@ void input_loop() {
 	continue;
       }
     }
+
     /// check that there aren't attempts for multiple of the same file redirection
     if (strchr(line, '<') != strrchr(line, '<') || strchr(line, '>') != strrchr(line, '>')) {
       perror("attempt for multiple of the same file redirection\n");
@@ -173,7 +174,7 @@ void input_loop() {
       token = strtok(NULL, space);
       tcount++;
     }
-    
+
     /// check that pipe and redirection characters only appear alone and not within a word or at the beginning or end of a token group
     if (strpbrk(tokens[0], controls) != NULL) {
       perror("illegal control character in first token\n");
@@ -182,7 +183,8 @@ void input_loop() {
     if (tcount > 1 && strpbrk(tokens[tcount-1], controls) != NULL) {
       perror("illegal control character in last token\n");
     }
-    /// count token groups, checking misplaced <,>,|
+
+    /// count token groups, checking for misplaced <,>,|
     groupcount = 1;
     err = 0;
     for (i = 0; i < tcount - 1; i++) {
@@ -217,101 +219,94 @@ void input_loop() {
       tokens[i] = '\0';
     }
 
-    // input redirect: extract input file and remove non-arg tokens
+    /// input redirect: extract input file and remove non-arg tokens
     if (in_redir) {
       for (i = 0; i < tcount - 1; i++) {
 	if (tokens[i][0] == '<') {
 	  strcpy(infile, tokens[i+1]);
-	  for (j = i; j < tcount - 2; j++) {
-	    strcpy(tokens[j], tokens[j+2]);
-	  }
-	  tokens[tcount - 1] = '\0';
-	  tokens[tcount - 2] = '\0';
-	  tcount -= 2;
+	  //for (j = i; j < tcount - 2; j++) {
+	  //strcpy(tokens[j], tokens[j+2]);
+	  //}
+	  //tokens[tcount - 1] = '\0';
+	  //tokens[tcount - 2] = '\0';
+	  //tcount -= 2;
+	  //break;
+	  tokens[i] = '\0';
+	  tokens[i+1] = '\0';
 	  break;
 	}
       }
     }
 
-    // output redirect: extract output file and remove non-arg tokens
+    /// output redirect: extract output file and remove non-arg tokens
     if (out_redir) {
       for (i = 0; i < tcount - 1; i++) {
 	if (tokens[i][0] == '>') {
 	  strcpy(outfile, tokens[i+1]);
-	  for (j = i; j < tcount - 2; j++) {
-	    strcpy(tokens[j], tokens[j+2]);
-	  }
-	  tokens[tcount - 1] = '\0';
-	  tokens[tcount - 2] = '\0';
-	  tcount -= 2;
+	  //for (j = i; j < tcount - 2; j++) {
+	  //strcpy(tokens[j], tokens[j+2]);
+	  //}
+	  //tokens[tcount - 1] = '\0';
+	  //tokens[tcount - 2] = '\0';
+	  //tcount -= 2;
+	  tokens[i] = '\0';
+	  tokens[i+1] = '\0';
 	  break;
 	}
       }
     }
 
     if (groupcount > 1) {
-      //printf("haven't done piping\n");
-      char** commands[50];
+      char commands[100][100][100];
       int cmdcount = 0;
-      char* cmd[50];
       int tok = 0, wordcount = 0, finish = 0;
-      
-      while (tokens[tok] != NULL && !finish) {
+      for (i = 0; i < tcount; i++) {
+	printf("%s ", tokens[i]);
+      }
+      while (!finish && tokens[tok] != NULL) {
+	char cmd[100][100];
 	wordcount = 0;
-	while (strcmp(tokens[tok], "|") != 0) {
-	  cmd[wordcount] = tokens[tok];
-	  tok++;
+	while (tok < tcount && strcmp(tokens[tok], "|") != 0) {
 	  if (tokens[tok] == NULL) {
 	    finish = 1;
-	    wordcount++;
-	    break;
+	    //wordcount++;
+	    //tok++;
+	    continue;
 	  }
+	  strcpy(cmd[wordcount], tokens[tok]);
+	  printf("copied %s\n", tokens[tok]);
+	  tok++;
 	  wordcount++;
 	}
-	cmd[wordcount] = NULL;
+	//cmd[wordcount] = NULL;
 	tok++;
-	commands[cmdcount] = cmd;
-	cmdcount++;
-      }
-      commands[cmdcount] = NULL;
       
-      /*char* token = strtok(line2, pipe);
-      i = 0;
-      while (token != NULL) {
-	tokengroups[i] = token;
-	token = strtok(NULL, pipe);
-	i++;
-      }
-      //printf("%i\n", groupcount);
-      for (j = 0; j < groupcount; j++) {
-	//printf("[%s]", tokengroups[j]);
-	char* tmp[15];
-	char* token = strtok(tokengroups[j], space);
-	i = 0;
-	while (token != NULL) {
-	  //tmp[i] = token;
-	  strcpy(tmp[i], token);
-	  token = strtok(NULL, space);
-	  i++;
+        /// copy cmd, string-by-string into commands[cmdcount]
+	printf("got here, wc = %i\n", wordcount);
+	for (i = 0; i < wordcount; i++) {
+	  printf("strcpy, i = %i, cmd[i] = %s\n", i, cmd[i]);
+	  strcpy(commands[cmdcount][i], cmd[i]);
 	}
-	tmp[i] = '\0';
-	//commands[j] = tmp;
-	memcpy(commands[j], tmp, sizeof(tmp));
+	//commands[cmdcount][wordcount] = "\0";
 	cmdcount++;
       }
-      commands[groupcount] = '\0';
-      */
+      //commands[cmdcount] = NULL;
+      
       pipe_exec(commands, groupcount, in_redir, out_redir, infile, outfile, tcount);
       continue;
     }
+
+    /// identify and execute commands with io redirection
     else if (in_redir || out_redir) {
       io_redir(tokens, in_redir, out_redir, infile, outfile, tcount);
       continue;
     }
-    /// now send the token groups to the command handler
+
+    /// otherwise: single command with no io redirection, execute normally
     cmd_exec(tokens, groupcount, tcount);
   }
 }
+
 int main(int argc, char* argv[]) {
   setenv("TERM", "msh", 0);
   input_loop();
